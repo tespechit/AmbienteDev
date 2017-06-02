0965ec711949b6fea3453ff0a32ab99931caced9
tmenegaz <tmenegaz77@gmail.com>
2017-06-02 15:59:41 -0300

    organizado

--- ambiente.sh
+++ ambiente.sh
@@ -3,36 +3,19 @@ clear
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 clear
 ## dependências para o sistema
-sudo apt install -y \
-build-essential \
-git \
-xclip \
-xsel \
-qt5-default \
-libqt5webkit5-dev \
-python-lxml \
-xvfb \
-mercurial \
-ffmpeg \
-libsdl1.2-dev \
-libsdl2-dev \
-libsmpeg-dev \
-libsdl-image1.2-dev \
-libsdl2-image-dev \
-libsdl-mixer1.2-dev \
-libsdl2-mixer-dev \
-libsdl-ttf2.0-dev \
-libsdl2-ttf-dev \
-libportmidi-dev \
-libswscale-dev \
-libavformat-dev \
-libavcodec-dev \
-zlib1g-dev
+sudo apt install -y build-essential git \
+xclip xsel qt5-default \
+libqt5webkit5-dev python-lxml xvfb \
+mercurial ffmpeg libsdl1.2-dev \
+libsdl2-dev libsmpeg-dev libsdl-image1.2-dev \
+libsdl2-image-dev libsdl-mixer1.2-dev libsdl2-mixer-dev \
+libsdl-ttf2.0-dev libsdl2-ttf-dev libportmidi-dev \
+libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
 clear
 
 sudo pip3 install --upgrade pip setuptools dryscrape beautifulsoup4
 clear
-echo "Baixar e instalar o Pycharm-community"
+# echo "Baixar e instalar o Pycharm-community"
 # read -p 'Pressione qualquer tecla para continuar: ' q
 touch /tmp/versao.py
 echo '
@@ -66,8 +49,8 @@ if test ${versaoNova} != ${versaoVelha}; then
     # read -p 'Pressione qualquer tecla para continuar: ' q
     # sleep 3
 else
-    echo 'Nenhuma alteração foi feita em no diretório /opt
-    O Pycharm-community é a versão mais recente\n: Pycharm-community-'${versaoNova}
+    echo 'Nenhuma alteração foi feita em no diretório /opt'
+    echo 'O Pycharm-community é a versão mais recente\n: Pycharm-community-'${versaoNova}
     # sleep 3
 fi
 
@@ -75,7 +58,7 @@ clear
 read -p "Qual o nome do novo projeto? " novoProjeto
 projeto=Projeto_${novoProjeto}
 # read -p "Qual o nome do ambiente virtual (virtualenv)? " venv
-# mkdir -p ~/PycharmProjects/${projeto}/src/{model/bd,view/tela,controller/logica}
+# mkdir -p ~/PycharmProjects/${projeto}/src/{model/bd,view/tela,controller/logica} 
 mkdir -p ~/PycharmProjects/${projeto}/src/model/bd
 mkdir -p ~/PycharmProjects/${projeto}/src/view/tela
 mkdir -p ~/PycharmProjects/${projeto}/src/controller/logica
@@ -91,6 +74,7 @@ if __name__ == '__main__':
     app.MyApp().run()
 " >> ~/PycharmProjects/${projeto}/src/main.py
 cd ~/PycharmProjects/${projeto}
+
 touch ~/PycharmProjects/${projeto}/src/myApp.py
 echo "
 import kivy
@@ -106,8 +90,6 @@ class MyApp(App):
         return Button(text='Olá mundo!')
 
 " >> ~/PycharmProjects/${projeto}/src/myApp.py
-export projeto
-clear
     
 clear
 echo "A preparar o ambiente de desenvolvomento Kivy para o projeto: "${projeto}" no abiente" ${novoProjeto}_venv
