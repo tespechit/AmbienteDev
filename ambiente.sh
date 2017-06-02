ac50101aeee740607177d102ce59b8b0fca85058
tmenegaz <tmenegaz77@gmail.com>
2017-06-01 23:32:59 -0300

    script sem interrupções do tipo sleep e outras. O script roda direto.

--- ambiente.sh
+++ ambiente.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
-sleep 3
+
 clear
 sudo apt install -y \
 build-essential \
@@ -13,11 +13,11 @@ libqt5webkit5-dev \
 python-lxml \
 xvfb
 clear
-sleep 3
+
 sudo pip3 install --upgrade pip setuptools dryscrape beautifulsoup4
 clear
 echo "Baixar e instalar o Pycharm-community"
-read -p 'Pressione qualquer tecla para continuar: ' q
+# read -p 'Pressione qualquer tecla para continuar: ' q
 touch ~/versao.py
 echo '
 import dryscrape
@@ -34,25 +34,25 @@ versaoNova=`python3 ~/versao.py`
 echo 'Versão nova: '${versaoNova}
 versaoVelha=`echo /opt/pycharm-community-* | sed "s/.*-//"`
 echo 'versão velha: '${versaoVelha}
-read -p 'Pressione qualquer tecla para continuar: ' q
+# read -p 'Pressione qualquer tecla para continuar: ' q
 if test ${versaoNova} != ${versaoVelha}; then
     wget -c https://download.jetbrains.com/python/pycharm-community-${versaoNova}.tar.gz
     sudo chmod +x pycharm-community-${versaoNova}.tar.gz
     sudo tar xvzf pycharm-community-${versaoNova}.tar.gz -C /opt/
     echo 'O Pycharm-community foi atualizado para a versão mais recente: Pycharm-community-'${versaoNova}
     echo /opt/pycharm-community-*
-read -p 'Pressione qualquer tecla para continuar: ' q
+# read -p 'Pressione qualquer tecla para continuar: ' q
     sudo rm -r ~/*.tar.gz
     sudo rm -r ~/versao.py
-    sleep 3
+    # sleep 3
     sudo rm -r /opt/pycharm-community-${versaoVelha}
     echo 'O Pycharm-community-'${versaoVelha}' foi removido com sucesso!'
-    read -p 'Pressione qualquer tecla para continuar: ' q
-    sleep 3
+    # read -p 'Pressione qualquer tecla para continuar: ' q
+    # sleep 3
 else
     echo 'Nenhuma alteração foi feita em no diretório /opt
     O Pycharm-community é a versão mais recente\n: Pycharm-community-'${versaoNova}
-    sleep 3
+    # sleep 3
 fi
 clear
 read -p "Qual o nome do novo projeto? " novoProjeto
@@ -62,41 +62,34 @@ mkdir -p ~/PycharmProjects/${projeto}
 cd ~/PycharmProjects/${projeto}
 export projeto
 clear
-read -p "Novo projeto Kivy? k p/(Kivy) ou c p/(cancela)  " opcao
-if test $opcao = 'k' || test $opcao = 'K' || test $opcao = 'kivy' || test $opcao = 'Kivy' || test $opcao = 'KIVY'; then
-    sudo apt install -y \
-    mercurial \
-    ffmpeg \
-    libsdl1.2-dev \
-    libsdl2-dev \
-    libsmpeg-dev \
-    libsdl-image1.2-dev \
-    libsdl2-image-dev \
-    libsdl-mixer1.2-dev \
-    libsdl2-mixer-dev \
-    libsdl-ttf2.0-dev \
-    libsdl2-ttf-dev \
-    libportmidi-dev \
-    libswscale-dev \
-    libavformat-dev \
-    libavcodec-dev \
-    zlib1g-dev    
-    clear
-    echo "A preparar o ambiente de desenvolvomento Kivy para o projeto: "${projeto}" no abiente" ${venv}
-    read -p 'Pressione qualquer tecla para continuar: ' q
-    virtualenv -p python3.5 $VIRTUALENVWRAPPER_HOOK_DIR/${venv}
-    ativa=$VIRTUALENVWRAPPER_HOOK_DIR/${venv}/bin/activate
-    export venv ativa
-    . ${ativa}
-    pip3 install cython==0.23
-    pip3 install --upgrade numpy kivy pygame urllib3 beautifulsoup4 lxml html5lib kivy-garden jupyter dryscrape
-    /opt/pycharm-community-*/bin/pycharm.sh ~/PycharmProjects/${projeto} &
-    sleep 3
-    jupyter notebook  ~/PycharmProjects/${projeto} &
-    exit 0
-elif test $opcao = 'c' || test $opcao = 'C' || test $opcao = 'cancela' || test $opcao = 'Cancela' || test $opcao = 'CANCELA'; then
-    sudo rm -r ~/${projeto}
-    exit 0
-else
-    echo 'opção inválida!'
-fi
+
+sudo apt install -y \
+mercurial \
+ffmpeg \
+libsdl1.2-dev \
+libsdl2-dev \
+libsmpeg-dev \
+libsdl-image1.2-dev \
+libsdl2-image-dev \
+libsdl-mixer1.2-dev \
+libsdl2-mixer-dev \
+libsdl-ttf2.0-dev \
+libsdl2-ttf-dev \
+libportmidi-dev \
+libswscale-dev \
+libavformat-dev \
+libavcodec-dev \
+zlib1g-dev    
+clear
+echo "A preparar o ambiente de desenvolvomento Kivy para o projeto: "${projeto}" no abiente" ${venv}
+# read -p 'Pressione qualquer tecla para continuar: ' q
+virtualenv -p python3.5 $VIRTUALENVWRAPPER_HOOK_DIR/${venv}
+ativa=$VIRTUALENVWRAPPER_HOOK_DIR/${venv}/bin/activate
+export venv ativa
+. ${ativa}
+pip3 install cython==0.23
+pip3 install --upgrade numpy kivy pygame urllib3 beautifulsoup4 lxml html5lib kivy-garden jupyter dryscrape
+/opt/pycharm-community-*/bin/pycharm.sh ~/PycharmProjects/${projeto} &
+# sleep 3
+jupyter notebook  ~/PycharmProjects/${projeto} &
+exit 0
