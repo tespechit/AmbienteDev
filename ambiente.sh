25093e58ca60f6c30b551cb44d02a974d6a473f9
tmenegaz <tmenegaz77@gmail.com>
2017-06-02 15:31:35 -0300

    atualizado para criar um projeto executável

--- ambiente.sh
+++ ambiente.sh
@@ -3,6 +3,7 @@ clear
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 
 clear
+## dependências para o sistema
 sudo apt install -y \
 build-essential \
 git \
@@ -11,59 +12,7 @@ xsel \
 qt5-default \
 libqt5webkit5-dev \
 python-lxml \
-xvfb
-clear
-
-sudo pip3 install --upgrade pip setuptools dryscrape beautifulsoup4
-clear
-echo "Baixar e instalar o Pycharm-community"
-# read -p 'Pressione qualquer tecla para continuar: ' q
-touch ~/versao.py
-echo '
-import dryscrape
-from bs4 import BeautifulSoup
-session = dryscrape.Session()
-session.visit("http://www.jetbrains.com/pycharm/download/#section=linux")
-response = session.body()
-bsObj = BeautifulSoup(response, "lxml")
-print(bsObj.findAll("ul", class_="g-col-12 _md-6 _sm-12 download__list-details").__str__()[118:126])
-' > ~/versao.py
-clear
-cat ~/versao.py
-versaoNova=`python3 ~/versao.py`
-echo 'Versão nova: '${versaoNova}
-versaoVelha=`echo /opt/pycharm-community-* | sed "s/.*-//"`
-echo 'versão velha: '${versaoVelha}
-# read -p 'Pressione qualquer tecla para continuar: ' q
-if test ${versaoNova} != ${versaoVelha}; then
-    wget -c https://download.jetbrains.com/python/pycharm-community-${versaoNova}.tar.gz
-    sudo chmod +x pycharm-community-${versaoNova}.tar.gz
-    sudo tar xvzf pycharm-community-${versaoNova}.tar.gz -C /opt/
-    echo 'O Pycharm-community foi atualizado para a versão mais recente: Pycharm-community-'${versaoNova}
-    echo /opt/pycharm-community-*
-# read -p 'Pressione qualquer tecla para continuar: ' q
-    sudo rm -r ~/*.tar.gz
-    sudo rm -r ~/versao.py
-    # sleep 3
-    sudo rm -r /opt/pycharm-community-${versaoVelha}
-    echo 'O Pycharm-community-'${versaoVelha}' foi removido com sucesso!'
-    # read -p 'Pressione qualquer tecla para continuar: ' q
-    # sleep 3
-else
-    echo 'Nenhuma alteração foi feita em no diretório /opt
-    O Pycharm-community é a versão mais recente\n: Pycharm-community-'${versaoNova}
-    # sleep 3
-fi
-clear
-read -p "Qual o nome do novo projeto? " novoProjeto
-projeto=Projeto_${novoProjeto}
-read -p "Qual o nome do ambiente virtual (virtualenv)? " venv
-mkdir -p ~/PycharmProjects/${projeto}
-cd ~/PycharmProjects/${projeto}
-export projeto
-clear
-
-sudo apt install -y \
+xvfb \
 mercurial \
 ffmpeg \
 libsdl1.2-dev \
@@ -79,12 +28,93 @@ libportmidi-dev \
 libswscale-dev \
 libavformat-dev \
 libavcodec-dev \
-zlib1g-dev    
+zlib1g-dev
 clear
-echo "A preparar o ambiente de desenvolvomento Kivy para o projeto: "${projeto}" no abiente" ${venv}
+
+sudo pip3 install --upgrade pip setuptools dryscrape beautifulsoup4
+clear
+echo "Baixar e instalar o Pycharm-community"
 # read -p 'Pressione qualquer tecla para continuar: ' q
-virtualenv -p python3.5 $VIRTUALENVWRAPPER_HOOK_DIR/${venv}
-ativa=$VIRTUALENVWRAPPER_HOOK_DIR/${venv}/bin/activate
+touch /tmp/versao.py
+echo '
+import dryscrape
+from bs4 import BeautifulSoup
+session = dryscrape.Session()
+session.visit("http://www.jetbrains.com/pycharm/download/#section=linux")
+response = session.body()
+bsObj = BeautifulSoup(response, "lxml")
+print(bsObj.findAll("ul", class_="g-col-12 _md-6 _sm-12 download__list-details").__str__()[118:126])
+' > /tmp/versao.py
+clear
+versaoNova=`python3 /tmp/versao.py`
+versaoVelha=`echo /opt/pycharm-community-* | sed "s/.*-//"`
+# read -p 'Pressione qualquer tecla para continuar: ' q
+if test ${versaoNova} != ${versaoVelha}; then
+    wget -c -q -P /tmp/ https://download.jetbrains.com/python/pycharm-community-${versaoNova}.tar.gz
+    wget -c -q -P /tmp/  https://github.com/Zen-CODE/kivybits/blob/master/IDE/PyCharm_kv_completion.jar
+    sudo chmod +x /tmp/pycharm-community-${versaoNova}.tar.gz
+    sudo chmod +x /tmp/PyCharm_kv_completion.jar
+    sudo tar xvzf /tmp/pycharm-community-${versaoNova}.tar.gz -C /opt/
+    
+    echo 'O Pycharm-community foi atualizado para a versão mais recente: Pycharm-community-'${versaoNova}
+    echo /opt/pycharm-community-*
+# read -p 'Pressione qualquer tecla para continuar: ' q
+    sudo rm -r /tmp/*.tar.gz
+    sudo rm -r /tmp/versao.py
+    # sleep 3
+    sudo rm -r /opt/pycharm-community-${versaoVelha}
+    echo 'O Pycharm-community-'${versaoVelha}' foi removido com sucesso!'
+    # read -p 'Pressione qualquer tecla para continuar: ' q
+    # sleep 3
+else
+    echo 'Nenhuma alteração foi feita em no diretório /opt
+    O Pycharm-community é a versão mais recente\n: Pycharm-community-'${versaoNova}
+    # sleep 3
+fi
+
+clear
+read -p "Qual o nome do novo projeto? " novoProjeto
+projeto=Projeto_${novoProjeto}
+# read -p "Qual o nome do ambiente virtual (virtualenv)? " venv
+# mkdir -p ~/PycharmProjects/${projeto}/src/{model/bd,view/tela,controller/logica}
+mkdir -p ~/PycharmProjects/${projeto}/src/model/bd
+mkdir -p ~/PycharmProjects/${projeto}/src/view/tela
+mkdir -p ~/PycharmProjects/${projeto}/src/controller/logica
+
+touch ~/PycharmProjects/${projeto}/src/main.py
+echo "
+import kivy
+kivy.require('1.7.0')
+
+import src.myApp as app
+
+if __name__ == '__main__':
+    app.MyApp().run()
+" >> ~/PycharmProjects/${projeto}/src/main.py
+cd ~/PycharmProjects/${projeto}
+touch ~/PycharmProjects/${projeto}/src/myApp.py
+echo "
+import kivy
+kivy.require('1.7.0')
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    title = 'Minha App'
+
+    def build(self):
+        return Button(text='Olá mundo!')
+
+" >> ~/PycharmProjects/${projeto}/src/myApp.py
+export projeto
+clear
+    
+clear
+echo "A preparar o ambiente de desenvolvomento Kivy para o projeto: "${projeto}" no abiente" ${novoProjeto}_venv
+# read -p 'Pressione qualquer tecla para continuar: ' q
+virtualenv -p python3.5 $VIRTUALENVWRAPPER_HOOK_DIR/${novoProjeto}_venv
+ativa=$VIRTUALENVWRAPPER_HOOK_DIR/${novoProjeto}_venv/bin/activate
 export venv ativa
 . ${ativa}
 pip3 install cython==0.23
